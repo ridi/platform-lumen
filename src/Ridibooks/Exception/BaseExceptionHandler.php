cccf4563f73b8da37e1951841e05dc3cd7b15aac
문정기 <jerrym@ridi.com>
2020-04-24 18:09:40 +0900

    remove function updateDontReport

--- src/Ridibooks/Exception/BaseExceptionHandler.php
+++ src/Ridibooks/Exception/BaseExceptionHandler.php
@@ -7,11 +7,6 @@ use Laravel\Lumen\Exceptions\Handler;
 
 class BaseExceptionHandler extends Handler
 {
-    protected function updateDontReport(array $dont_report_exceptions = [])
-    {
-        $this->dontReport = $dont_report_exceptions;
-    }
-
     public function report(\Throwable $e)
     {
         if (!$this->shouldntReport($e)) {
