851859181fb8c681b41a72503f5764ae13092f5c
문정기 <jerrym@ridi.com>
2020-04-24 18:07:20 +0900

    update report and add function callAdditional  in BaseExceptionHandler

--- src/Ridibooks/Exception/BaseExceptionHandler.php
+++ src/Ridibooks/Exception/BaseExceptionHandler.php
@@ -11,4 +11,23 @@ class BaseExceptionHandler extends Handler
     {
         $this->dontReport = $dont_report_exceptions;
     }
+
+    public function report(\Throwable $e)
+    {
+        if (!$this->shouldntReport($e)) {
+            if (method_exists($e, 'report')) {
+                return $e->report();
+            }
+
+            $this->callAdditional($e);
+
+            if (env('APP_DEBUG', false)) {
+                parent::report($e);
+            }
+        }
+    }
+
+    protected function callAdditional(\Throwable $e): void
+    {
+    }
 }
